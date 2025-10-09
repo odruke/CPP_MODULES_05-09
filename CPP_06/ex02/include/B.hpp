26fd6758fa49bb9debc4f6aebb160b5b5b3098de
odruke-s <odruke-s@student.42lausanne.ch>
2025-10-09 16:30:15 +0200

    finished ex02

---
+++ CPP_06/ex02/include/B.hpp
@@ -0,0 +1,9 @@
+#ifndef B_HPP
+# define B_hpp
+
+#include "Base.hpp"
+
+class B : public Base {};
+
+
+#endif
--- CPP_06/ex02/src/main.cpp
+++ CPP_06/ex02/src/main.cpp
@@ -1,106 +1,73 @@
-#include "Bureaucrat.hpp"
-#include "AForm.hpp"
-#include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "PresidentialPardonForm.hpp"
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 #include "colors.hpp"
+#include <ctime>
+#include <cstdlib>
+
+int randomNumber(int max)
+{
+	static bool seeded = false;
+
+	if (!seeded) {
+		srand(time(NULL));
+		seeded = true;
+	}
+	return rand() % (max + 1);
+}
+
+Base*	generate(void)
+{
+	int i = randomNumber(2);
+	if (i == 0)
+		return new A;
+	if (i == 1)
+		return new B;
+	if (i == 2)
+		return new C;
+	return NULL;
+}
+void identify(Base* p)
+{
+	A* a = dynamic_cast<A*>(p);
+	B* b = dynamic_cast<B*>(p);
+	C* c = dynamic_cast<C*>(p);
+
+	std::string realClass = a != NULL ? "A" : (b != NULL? "B" : (c != NULL? "C" : "unknown"));
+	std::cout << "i'm a pointer to class " << GREEN << realClass << RESET << std::endl;
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		A& a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "i'm a reference to class "
+				<< GREEN << "A" << RESET << std::endl;
+	}catch(const std::exception &e){}
+	try
+	{
+		B& b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "i'm a reference to class "
+				<< GREEN << "B" << RESET << std::endl;
+	}catch(const std::exception &e) {}
+	try
+	{
+		C& c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "i'm a reference to class "
+				<< GREEN << "C" << RESET << std::endl;
+	}catch(const std::exception &e) {}
+}
 
 int	main()
 {
-//declare bureaucrats and forms
-	Bureaucrat	hermes("Hermes Conrad", 34);
-	Bureaucrat	numberone("Number 1.0", 1);
-	Bureaucrat	morgan("Morgan Proctor", 19);
-	Bureaucrat	vogel("Warden Vogel", 135);
+	Base*	misteryClass = generate();
+	identify(misteryClass);
+	identify(*misteryClass);
 
-	ShrubberyCreationForm		plantSomeTree("acacia");
-	RobotomyRequestForm			robotomy("Bender");
-	PresidentialPardonForm		pardon("Philip J. fry");
-
-//announce bureaucrats
-	std::cout << "\nA bureaucrat named " << MAGENTA << hermes.getName().c_str() << RESET
-			<< " with grade " << ORANGE << hermes.getGrade() << RESET<< " has been instantiated" << std::endl;
-	std::cout << "A bureaucrat named " << MAGENTA << numberone.getName().c_str() << RESET
-			<< " with grade " << ORANGE << numberone.getGrade() << RESET <<" has been instantiated" << std::endl;
-	std::cout << "A bureaucrat named " << MAGENTA << vogel.getName().c_str() << RESET
-			<< " with grade " << ORANGE << vogel.getGrade() << RESET <<" has been instantiated" << std::endl;
-	std::cout << "A bureaucrat named " << MAGENTA << morgan.getName().c_str() << RESET
-			<< " with grade " << ORANGE << morgan.getGrade() << RESET <<" has been instantiated" << std::endl;
-
-	std::cout << std::endl;
-
-//announce forms
-	std::cout << "Form " << plantSomeTree << " needs a "
-			<< GREEN << plantSomeTree.getGradeToSign() << RESET
-			<<" level bureaucrat to sign it, and a "
-			<< GREEN << plantSomeTree.getGradeToExec() << RESET
-			<< " to execute it.\n";
-	std::cout << "Form " << robotomy << " needs a "
-			<< GREEN << robotomy.getGradeToSign() << RESET
-			<< " level bureaucrat to sign it, and a "
-			<< GREEN << robotomy.getGradeToExec() << RESET
-			<< " to execute it.\n";
-	std::cout << "Form " << pardon << " needs a "
-			<< GREEN << pardon.getGradeToSign() << RESET
-			<< " level bureaucrat to sign it, and a "
-			<< GREEN << pardon.getGradeToExec() << RESET
-			<< " to execute it.\n";
-
-//degrade hermes because
-	while (hermes.getGrade() < 150)
-		hermes.decrementGrade();
-	std::cout << "\n" << hermes << " has been degraded" << std::endl;
-
-/* ============== test 1 ============= */
-	std::cout << YELLOW << "\n============== test 1: try to execute non signed contracts ============= \n" << RESET << std::endl;
-
-//checking that forms are in fact unsigned
-	std::cout << "Form " << plantSomeTree << " is currently " << (plantSomeTree.getIsSigned()? "signed" : "unsigned") << std::endl;
-	std::cout << "Form " << robotomy << " is currently " << (robotomy.getIsSigned()? "signed" : "unsigned") << std::endl;
-	std::cout << "Form " << pardon << " is currently " << (pardon.getIsSigned()? "signed\n" : "unsigned\n") << std::endl;
-
-
-//trying to execute forms and checking they are still unsigned after
-	hermes.executeForm(plantSomeTree);
-	std::cout << "Form " << plantSomeTree << " is currently " << (plantSomeTree.getIsSigned()? "signed\n" : "unsigned\n") << std::endl;
-
-	numberone.executeForm(plantSomeTree);
-	std::cout << "Form " << plantSomeTree << " is currently " << (plantSomeTree.getIsSigned()? "signed\n" : "unsigned\n") << std::endl;
-
-	morgan.executeForm(robotomy);
-	std::cout << "Form " << robotomy << " is currently " << (robotomy.getIsSigned()? "signed\n" : "unsigned\n") << std::endl;
-
-	morgan.executeForm(pardon);
-	std::cout << "Form " << pardon << " is currently " << (pardon.getIsSigned()? "signed\n" : "unsigned\n") << std::endl;
-
-
-
-/* ============== test 2 ============= */
-	std::cout << YELLOW << "\n============== test 2: sign and execute ============= \n" << RESET << std::endl;
-
-	std::cout << YELLOW << "Signing contracts" << RESET << std::endl;
-	std::cout << "Form type: " << GREEN << "ShrubberyCreationForm" << RESET << std::endl;
-	numberone.signForm(plantSomeTree);
-	hermes.signForm(plantSomeTree);
-
-	std::cout << "\nForm type: " << GREEN << "RobotomyRequestForm" << RESET << std::endl;
-	vogel.signForm(robotomy);
-	morgan.signForm(robotomy);
-
-	std::cout << "\nForm type: " << GREEN << "PresidentialPardonForm" << RESET << std::endl;
-	morgan.signForm(pardon);
-	numberone.signForm(pardon);
-
-	std::cout << YELLOW << "\nExecuting contracts" << RESET << std::endl;
-	std::cout << "Form type: " << GREEN << "ShrubberyCreationForm" << RESET << std::endl;
-	hermes.executeForm(plantSomeTree);
-	numberone.executeForm(plantSomeTree);
-
-	std::cout << "\nForm type: " << GREEN << "RobotomyRequestForm" << RESET << std::endl;
-	morgan.executeForm(robotomy);
-	vogel.executeForm(robotomy);
-
-	std::cout << "\nForm type: " << GREEN << "PresidentialPardonForm" << RESET << std::endl;
-	morgan.executeForm(pardon);
-	numberone.executeForm(pardon);
+	delete misteryClass;
 }
